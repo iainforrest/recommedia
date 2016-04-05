5ccc422219424ee915e1a310fe52f0657fd90838
IForrest <iainforrest@me.com>
2016-04-05 22:32:37 +1200

    cleaned up working directory
    Former-commit-id: 2c47f18fb41d7ef00bf2403cf965c3af6974d99a

--- offline/html/Jamie Foxx on Workout Routines, Success Habits, and Untold Hollywood Stories _ The Blog of Author Tim Ferriss_files/init1.js
+++
@@ -1,9 +0,0 @@
-//<script>
-bouncex.state = {"newvid":false,"iol":false,"tn":1454351168,"rc":false,"mobile":false,"vip":"111.69.113.239"};
-bouncex.cookie = {"fvt":1441707907,"vid":1454351124667126,"ao":22,"as":1,"vpv":3,"d":"d","lp":"http%3A%2F%2Ffourhourworkweek.com%2Fpodcast%2F","r":"fourhourworkweek.com","cvt":1454351124,"gcr":98,"m":0,"sid":0,"campaigns":{"208748":{"vv":1,"lvt":1454349223,"lavid":1454348832320306,"la":1454348835,"fsa":1450604479,"av":1,"as":1,"ao":10},"221837":{"vv":1,"lvt":1454351125,"lavid":1454351124667126,"la":1454351125,"fsa":1452880414,"av":1,"as":1,"ao":5},"224794":{"vv":1,"lvt":1454351141,"lavid":1454351124667126,"la":1454351141,"fsa":1453019982,"av":1,"as":1,"ao":4,"wc":1454228997},"224796":{"vv":2,"lvt":1453453082,"lavid":1453452469518102,"la":1453452962,"fsa":1452880444,"av":2,"as":1,"ao":4,"wc":1453453494}},"lvt":1454351168,"ibxt":"MTQ0MTcwNzUxMzIxOTQxMQ%3D%3D"};
-bouncex.brandStyles = false;
-bouncex.webfonts = false;
-bouncex.campaigns = false;
-bouncex.debug = false;
-
-bouncex.init();
